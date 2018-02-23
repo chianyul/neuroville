ee5e47d3f0bcd1eed03fe68ec9beb9c78f29d51c
Chianyu Liu <qianyu.liu@mail.mcgill.ca>
2018-02-23 13:23:40 -0500

    Completed network and filters

--- feedbackMail.php
+++ feedbackMail.php
@@ -1,7 +1,8 @@
 <?php
 	// The message
 	$name = $_POST['name'];
-	$emailTo = "qianyu.liu@mail.mcgill.ca";
+	//$emailTo = "qianyu.liu@mail.mcgill.ca";
+	$emailTo = "chainyu.liu@outlook.com";
 	$emailFrom = $_POST['email'];
 	$subject = "Feedback Test from $emailFrom";
 	$message = $_POST['message'];
